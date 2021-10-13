f4d5902cb2c78bde7511c490c3abc63e4fce2394
johnGachihi <johngachihi3@gmail.com>
2021-10-13 14:55:49 +0300

    Add page-template component

---
+++ src/components/page-template/page-template.tsx
@@ -0,0 +1,24 @@
+import Drawer from "./drawer"
+import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
+import { PropsWithChildren } from "react"
+
+type Props = PropsWithChildren<{ title?: string }>
+
+function PageTemplate({ children, title }: Props) {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Drawer />
+      <Box sx={{ p: 3 }}>
+        {title && (
+          <Box sx={{ mb: 2 }}>
+            <Typography variant="h5">{title}</Typography>
+          </Box>
+        )}
+        {children}
+      </Box>
+    </Box>
+  )
+}
+
+export default PageTemplate
